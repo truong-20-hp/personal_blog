4ac488a9bbf7c53b90ee67eab7625ac24f2ec4a9
xuantruong20 <01289258365a@gmail.com>
2023-08-23 18:29:04 +0700

    update lan2

--- asset/slick.js
+++ asset/slick.js
@@ -1,6 +1,7 @@
 $ (document).ready(function(){
     $(".event__content__img--slick") .slick ({
         slidesToShow: 2,
+        dots:true,
         prevArrow:'<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="display: block;" fdprocessedid="s57fd8"><i class="slick-arrow fa-solid fa-chevron-left"></i></button>',
         nextArrow:'<button class="slick-next slick-arrow" aria-label="Next" type="button" style="display: block;" fdprocessedid="w6hm7m"><i class="slick-arrow fa-solid fa-chevron-right"></i></button>'
     })
